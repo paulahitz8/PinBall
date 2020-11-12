a8c3b44b29fd25a2581204d445e56a83b642e120
Paula Hitz <59050082+paulahitz8@users.noreply.github.com>
2020-11-12 21:05:28 +0100

    en fin

---
+++ ModulePlayer.cpp
@@ -0,0 +1,54 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModulePlayer.h"
+#include "ModuleRender.h"
+#include "ModulePhysics.h"
+#include "ModuleTextures.h"
+#include "ModuleInput.h"
+#include "ModuleAudio.h"
+#include "ModuleSceneIntro.h"
+#include "Box2D/Box2D/Box2D.h"
+
+ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
+{}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player");
+
+	right1FlipperTex = App->textures->Load("pinball/flipperdownright.png");
+	right2FlipperTex = App->textures->Load("pinball/flipperupright.png");
+	leftFlipperTex = App->textures->Load("pinball/flipperdownleft.png");
+	ballTex = App->textures->Load("pinball/ball.png");
+
+	posInitial = { 566, 815 };
+
+	return true;
+}
+
+// Unload assets
+bool ModulePlayer::CleanUp()
+{
+	LOG("Unloading player");
+
+	App->textures->Unload(right1FlipperTex);
+	App->textures->Unload(right2FlipperTex);
+	App->textures->Unload(leftFlipperTex);
+
+	return true;
+}
+
+// Update: draw background
+update_status ModulePlayer::Update()
+{
+
+
+	return UPDATE_CONTINUE;
+}
+
+
+
---
+++ ModulePlayer.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "p2List.h"
+
+#include "Box2D/Box2D/Box2D.h"
+
+struct SDL_Texture;
+class PhysBody;
+
+class ModulePlayer : public Module
+{
+public:
+	ModulePlayer(Application* app, bool start_enabled = true);
+	virtual ~ModulePlayer();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	uint lifeCount = 3;
+	
+	bool isDead = false;
+	bool waterSlide = false;
+
+	SDL_Texture* right1FlipperTex = nullptr;
+	SDL_Texture* right2FlipperTex = nullptr;
+	SDL_Texture* leftFlipperTex = nullptr;
+	SDL_Texture* ballTex = nullptr;
+
+	SDL_Rect right1FlipperRect = { 307, 804, 104, 47 };
+	SDL_Rect right2FlipperRect = { 470, 328, 79, 73 };
+	SDL_Rect leftFlipperRect = { 176, 805, 104, 46 };
+
+	iPoint posInitial;
+	iPoint pos;
+
+	PhysBody* ball = nullptr;
+
+	PhysBody* propeller1 = nullptr;
+	PhysBody* propeller2 = nullptr;
+	PhysBody* propeller3 = nullptr;
+
+	PhysBody* propeller1_pivot = nullptr;
+	PhysBody* propeller2_pivot = nullptr;
+	PhysBody* propeller3_pivot = nullptr;
+
+	b2PrismaticJoint* propeller1Joint;
+	b2PrismaticJoint* propeller2Joint;
+	b2PrismaticJoint* propeller3Joint;
+
+	PhysBody* right1Flipper = nullptr;
+	PhysBody* right2Flipper = nullptr;
+	PhysBody* leftFlipper = nullptr;
+
+	PhysBody* right1Flipper_pivot = nullptr;
+	PhysBody* right2Flipper_pivot = nullptr;
+	PhysBody* leftFlipper_pivot = nullptr;
+	 
+	b2RevoluteJoint* right1FlipperJoint = nullptr;
+	b2RevoluteJoint* right2FlipperJoint = nullptr;
+	b2RevoluteJoint* leftFlipperJoint = nullptr;
+};
